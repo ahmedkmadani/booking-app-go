3f92285f2e498b931c541d32bb890f9ab0c80e3e
Ahmed K Madani <ahmedkamal543@gmail.com>
2022-03-01 01:56:35 +0300

    use printf

--- main.go
+++ main.go
@@ -3,6 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Welcome to our conference booking application")
+
+	var conferenceName = "Go conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Printf("Welcome to %v booking application \n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still availabe \n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your ticket here to attend")
+
 }
